89781232c1b51c2870721422377eb2c1f35a1f22
Jeff Prestes <jeffprestes@gmail.com>
2015-10-03 10:16:18 -0400

    Add optionals parameters

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ var Beacon = require('./node_modules/eddystone-beacon/lib/beacon');
 
 beacon = new Beacon();
 
-beacon.advertiseUrl('http://www.paypal.com');
+var options = {
+  txPowerLevel: -22,  // override TX Power Level, default value is -21,
+  tlmCount: 2,       // 2 TLM frames
+  tlmPeriod: 10      // every 10 advertisements
+};
+
+beacon.advertiseUrl('http://www.paypal.com', [options]);
